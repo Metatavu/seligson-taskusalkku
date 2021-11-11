e6f4422faca4e1f2f1922fb44c7526bd66baa74c
Valmetti <vmvalonen@gmail.com>
2021-11-11 13:07:17 +0200

    added back navigati0n to fund details

--- src/components/screens/funds/funds-details-screen.tsx
+++ src/components/screens/funds/funds-details-screen.tsx
@@ -1,17 +1,21 @@
+
 import React from "react";
 import { ScrollView, View } from "react-native";
 import FundCard from "../../generic/fund-card";
 import FundChart from "../../generic/fund-chart";
 import FundDetails from "../../generic/fund-details";
-import styles from "../../../styles/screens/funds/fund-screen";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import FundsNavigator from "../../../types/navigators/funds";
+import { Button, Text } from "react-native-paper";
+import strings from "../../../localization/strings";
+import styles from "../../../styles/screens/funds/funds-details-screen";
 
 /**
  * Funds details screen
  */
 const FundsDetailsScreen: React.FC = () => {
   const { params } = useRoute<FundsNavigator.RouteProps>();
+  const navigation = useNavigation<FundsNavigator.NavigationProps>();
 
   if (!params?.fund) return null;
 
@@ -19,13 +23,25 @@ const FundsDetailsScreen: React.FC = () => {
    * Component render
    */
   return (
-    <ScrollView>
-      <FundChart fund={ params.fund }/>
-      <View style={ styles.detailsWrapper }>
-        <FundCard fund={ params.fund }/>
-        <FundDetails fund={ params.fund }/>
-      </View>
-    </ScrollView>
+    <>
+      <Button
+        icon="arrow-left-circle"
+        onPress={ () => navigation.goBack() }
+        labelStyle={{ color: "#fff" }}
+        style={ styles.backButton }
+      >
+        <Text style={{ color: "#fff" }}>
+          { strings.generic.back }
+        </Text>
+      </Button>
+      <ScrollView>
+        <FundChart fund={ params.fund }/>
+        <View style={ styles.detailsWrapper }>
+          <FundCard fund={ params.fund }/>
+          <FundDetails fund={ params.fund }/>
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
