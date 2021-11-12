edb05d7cab174297bf8e66b793f08f7a3eb970ef
Veli-Matti Valonen <76946183+Valmetti@users.noreply.github.com>
2021-11-12 14:48:07 +0200

    Update src/components/screens/funds/passive-funds-screen.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/screens/funds/passive-funds-screen.tsx
+++ src/components/screens/funds/passive-funds-screen.tsx
@@ -21,9 +21,11 @@ const PassiveFundsScreen: React.FC = () => {
 
   /**
    * Render fund
+   *
+   * @param fund fund
    */
   const renderFund = (fund: Fund) => (
-    <TouchableOpacity onPress={ () => navigation.navigate("fundsDetails", { fund: fund })}>
+    <TouchableOpacity onPress={ () => navigation.navigate("fundsDetails", { fund: fund }) }>
       <FundCard fund={ fund }/>
     </TouchableOpacity>
   );
