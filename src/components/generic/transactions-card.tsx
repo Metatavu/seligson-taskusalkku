25c0a3f336338e518a985452e5bffb9f46dd9c7d
Valmetti <vmvalonen@gmail.com>
2021-12-08 10:47:16 +0200

    tranasactions screen

---
+++ src/components/generic/transactions-card.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import { View } from "react-native";
+import { Divider, Text, useTheme } from "react-native-paper";
+import theme from "../../theme";
+import transactionsCardStyles from "../../styles/generic/transactions-card";
+import strings from "../../localization/strings";
+import { PortfolioTransaction, Fund } from "../../generated/client";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import auth from "../../utils/auth";
+import { FundsApiContext } from "../providers/funds-api-provider";
+import { ErrorContext } from "../error-handler/error-handler";
+import GenericUtils from "../../utils/generic";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+/**
+ * Component properties
+ */
+interface Props {
+  transactionTitle: String;
+  portfolioTransactions: PortfolioTransaction[];
+}
+
+/**
+ * Transactions card
+ *
+ * @param props component properties
+ */
+const TransactionsCard: React.FC<Props> = ({ transactionTitle, portfolioTransactions }) => {
+  const styles = transactionsCardStyles(useTheme(), "#FFF" || "#FFF");
+  const sortedTransactions = portfolioTransactions.sort((a, b) => b.paymentDate.getTime() - a.paymentDate.getTime());
+  
+  const fundsApiContext = React.useContext(FundsApiContext);
+  const errorContext = React.useContext(ErrorContext);
+  
+  const [ funds, setFunds ] = React.useState<Fund[]>([]);
+  const [closed, setClosed] = React.useState(true);
+  
+  /**
+   * Loads funds from API
+   */
+  const loadFunds = async () => {
+    if (!auth) {
+      return;
+    }
+
+    try {
+      /** TODO: add pagination support */
+      setFunds(await fundsApiContext.listFunds({ maxResults: 200 }));
+    } catch (error) {
+      errorContext.setError(strings.errorHandling.funds.list, error);
+    }
+  };
+
+  /**
+   * Effect for loading funds
+   */
+  React.useEffect(() => { loadFunds(); }, []);
+
+  /**
+   * Transaction value
+   *
+   * @param label
+   * @param value 
+   */
+  const renderTransactionValue = (label: string, value: number) => {
+    return (
+      <View style={ styles.shareColumn }>
+        <Text style={ styles.labelText }>
+          { label }
+        </Text>
+        <Text>
+          { value.toFixed(4) }
+          { label !== strings.fundDetailsScreen.amount ? "€" : "kpl" }
+        </Text>
+      </View>
+    );
+  };
+
+  /**
+   * Render transaction
+   */
+  const renderTransaction = (portfolioTransaction: PortfolioTransaction) => {
+    const { fundId, id, paymentDate, marketValue, shareAmount } = portfolioTransaction;
+    const i = funds.findIndex(fund => fund.id === fundId);
+    const { color, longName } = funds[i];
+
+    return (
+      <TouchableOpacity key={ id }>
+        <View style={ styles.transactionWrapper }>
+          <View style={[ styles.colorBar, { backgroundColor: color } ]}/>
+          <View style={ styles.transactionContent }>
+            <View style={ styles.transactionTitle }>
+              <Text style={[ theme.fonts.medium, { flex: 1 } ]}>
+                { longName && GenericUtils.getLocalizedValue(longName) }
+              </Text>
+              <Text style={ styles.labelText }>
+                { paymentDate.toLocaleDateString() }
+              </Text>
+            </View>
+            <Divider style={{ marginVertical: 5 }}/>
+            <View style={ styles.cardRow }>
+              { renderTransactionValue(strings.portfolio.transactions.rate, marketValue) }
+              { renderTransactionValue(strings.fundDetailsScreen.amount, shareAmount) }
+              { renderTransactionValue(strings.portfolio.statistics.total, (marketValue * shareAmount)) }
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+
+    );
+  };
+
+  /**
+   * Render transactions list
+   */
+  const RenderTransactionList = () => {
+    return (
+      <View style={ styles.transactionsList }>
+        { sortedTransactions.map(renderTransaction) }
+      </View>
+    );
+  };
+    
+  /**
+   * Component render
+   */
+  return (
+    <>
+      <View style={ styles.cardWrapper }>
+        <View style={ styles.cardContent }>
+          <TouchableOpacity
+            onPress={ () => (!closed ? setClosed(true) : setClosed(false)) }
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={[ theme.fonts.medium, { fontSize: 16 } ]} >
+              { transactionTitle }
+            </Text>
+            <Icon name="angle-down" size={ 20 }/>
+          </TouchableOpacity>
+          { closed ? <RenderTransactionList/> : null }
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default TransactionsCard;
--- src/components/screens/portfolio/events-screen.tsx
+++ src/components/screens/portfolio/events-screen.tsx
@@ -1,25 +1,95 @@
 import React from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import TransactionsCard from "../../generic/transactions-card";
+import theme from "../../../theme";
+import { ScrollView } from "react-native-gesture-handler";
+import { Portfolio, PortfolioTransaction, TransactionType } from "../../../generated/client";
+import { useAppSelector } from "../../../app/hooks";
+import { selectAuth } from "../../../features/auth/auth-slice";
+import { ErrorContext } from "../../error-handler/error-handler";
 import strings from "../../../localization/strings";
+import { PortfolioTransactionsApiContext } from "../../providers/portfolio-transactions-api-provider";
+import { PortfolioContext } from "./portfolio-context-provider";
 
 /**
  * Events screen
  */
 const EventsScreen: React.FC = () => {
+  const auth = useAppSelector(selectAuth);
+
+  const errorContext = React.useContext(ErrorContext);
+  const portfolioContext = React.useContext(PortfolioContext);
+  const portfolioTransactionsContext = React.useContext(PortfolioTransactionsApiContext);
+
+  const [ transactions, setTransactions ] = React.useState<PortfolioTransaction[]>([]);
+
+  /**
+ * Loads transactions from API
+ */
+  const loadTransactions = async (selectedPortfolio: Portfolio) => {
+    if (!auth || !selectedPortfolio.id) {
+      return;
+    }
+
+    try {
+      /** TODO: add pagination support */
+      setTransactions(await portfolioTransactionsContext.listPortfolioTransactions({ portfolioId: selectedPortfolio.id }));
+    } catch (error) {
+      errorContext.setError(strings.errorHandling.funds.list, error);
+    }
+  };
+
+  /**
+   * Effect for loading transactiosn
+   */
+  React.useEffect(() => {
+    const { selectedPortfolio } = portfolioContext;
+    if (!selectedPortfolio) {
+      return;
+    }
+
+    loadTransactions(selectedPortfolio);
+  }, [ portfolioContext.selectedPortfolio ]);
+  
+  /**
+   * Redemption transactions
+   */
+  const filteredTransactionsRedemptions = () => (
+    <TransactionsCard
+      key="transactionsCardRedemptions"
+      transactionTitle={ strings.portfolio.statistics.redemptions }
+      portfolioTransactions={ transactions.filter(transaction => transaction.type === TransactionType.Redemption) }
+    />
+  );
+
+  /**
+   * Subscription transactions
+   */
+  const filteredTransactionsSubscriptions = () => (
+    <TransactionsCard
+      key="transactionsCardSubscriptions"
+      transactionTitle={ strings.portfolio.statistics.subscriptions }
+      portfolioTransactions={ transactions.filter(transaction => transaction.type === TransactionType.Subscription) }
+    />
+  );
+
   /**
    * Component render
    */
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <Text>{ strings.generic.notImplemented }</Text>
-    </View>
+    <ScrollView>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-start",
+          alignItems: "center",
+          padding: theme.spacing(2)
+        }}
+      >
+        { filteredTransactionsRedemptions() }
+        { filteredTransactionsSubscriptions() }
+      </View>
+    </ScrollView>
   );
 };
 
