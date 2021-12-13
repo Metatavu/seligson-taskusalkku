d5503ac171d7764194d7f5c85e3ac35aba6be0c7
Valmetti <vmvalonen@gmail.com>
2021-12-13 11:23:55 +0200

    styling changes

--- src/components/screens/auth/login-required-screen.tsx
+++ src/components/screens/auth/login-required-screen.tsx
@@ -3,40 +3,47 @@ import React from "react";
 import { Linking, View } from "react-native";
 import { Button, Text } from "react-native-paper";
 import strings from "../../../localization/strings";
-import theme from "../../../theme";
 import RootNavigator from "../../../types/navigators/root";
+import styles from "../../../styles/screens/auth/login-required-screen";
+import SeligsonLogo from "../../../../assets/seligsonLogo";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 /**
  * Login required screen component
  *
- * TODO: Fix styling
  */
 const LoginRequiredScreen: React.FC = () => {
   const navigation = useNavigation<RootNavigator.NavigationProps>();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <View>
-        <Text style={{ textAlign: "center", padding: theme.spacing(3) }}>
-          { strings.auth.loginRequired }
-        </Text>
-      </View>
-      <Button onPress={ () => navigation.navigate("authentication", { screen: "login" }) }>
-        { strings.generic.login }
-      </Button>
-      <Button onPress={ () => navigation.navigate("authentication", { screen: "login", params: { demoLogin: true } }) }>
-        { strings.generic.login }
-      </Button>
-      <View>
-        <Text style={{ textAlign: "center", padding: theme.spacing(2) }}>
-          { strings.auth.newAccount }
-        </Text>
-        <Text
-          onPress={ () => Linking.openURL("https://www.seligson.fi/sco/suomi/asiointi/omasalkku/") }
-          style={{ textAlign: "center", textDecorationLine: "underline" }}
-        >
-          { strings.generic.webPage }
-        </Text>
+    <View style={ styles.loginScreen }>
+      <View style={ styles.cardWrapper }>
+        <View style={ styles.cardContent }>
+          <TouchableOpacity
+            onPress={ () => navigation.navigate("authentication", { screen: "login", params: { demoLogin: true } }) }
+          >
+            <SeligsonLogo/>
+          </TouchableOpacity>
+          <Text style={ styles.titleText }>
+            { strings.auth.loginRequired }
+          </Text>
+          <Button
+            onPress={ () => navigation.navigate("authentication", { screen: "login" }) }
+            style={ styles.loginButton }
+            labelStyle={{ color: "#fff" }}
+          >
+            { strings.generic.login }
+          </Button>
+          <Text style={{ textAlign: "center" }}>
+            { strings.auth.newAccount }
+          </Text>
+          <Text
+            onPress={ () => Linking.openURL("https://www.seligson.fi/sco/suomi/asiointi/omasalkku/") }
+            style={ styles.linkText }
+          >
+            { strings.generic.webPage }
+          </Text>
+        </View>
       </View>
     </View>
   );
