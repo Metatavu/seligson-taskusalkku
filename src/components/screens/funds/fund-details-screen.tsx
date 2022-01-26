5d3597646f1d6af375bf5e009ad702235686465a
Valmetti <vmvalonen@gmail.com>
2022-01-26 19:18:13 +0200

    cleanup

--- src/components/screens/funds/fund-details-screen.tsx
+++ src/components/screens/funds/fund-details-screen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { ActivityIndicator, ScrollView, View } from "react-native";
+import { ActivityIndicator, ScrollView, View, Text } from "react-native";
 import FundCard from "../../generic/fund-card";
 import DataChart from "../../generic/data-chart";
 import FundDetails from "../../generic/fund-details";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import FundsNavigator from "../../../types/navigators/funds";
-import { Button, Text } from "react-native-paper";
+import { Button } from "react-native-paper";
 import strings from "../../../localization/strings";
 import styles from "../../../styles/screens/funds/funds-details-screen";
 import { ChartRange, VictoryChartData } from "../../../types";
@@ -49,8 +49,6 @@ const FundDetailsScreen: React.FC = () => {
       const securities = await securitiesContext.listSecurities({ maxResults: 20000 });
       const fundSecurities = securities.filter(security => security.fundId === fund.id);
       const aSecurity = fundSecurities.find(security => security.name.fi.includes("(A)"));
-
-      console.log("a", fundSecurities);
 
       if (!aSecurity?.id) {
         throw new Error("Could not find A security!");
