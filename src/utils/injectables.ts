d0991eb004780317ee4cc0bd190e53f8261a80ed
Jari Nykänen <45241047+jarinykanen@users.noreply.github.com>
2022-01-27 13:08:52 +0200

    Update src/utils/injectables.ts
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/utils/injectables.ts
+++ src/utils/injectables.ts
@@ -10,12 +10,14 @@ namespace Injectables {
    *
    * TODO: Add html elements which would solve this during render
    */
-  export const iosWebviewZoomFix = "const meta = document.createElement('meta'); " +
-    "meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1'); " +
-    "meta.setAttribute('name', 'viewport'); " +
-    "document.getElementsByTagName('head')[0].appendChild(meta); " +
-    "window.ReactNativeWebView.postMessage(document.body.scrollHeight);" +
-    "true; ";
+  export const iosWebviewZoomFix = `
+    const meta = document.createElement("meta");
+    meta.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1");
+    meta.setAttribute("name", "viewport");
+    document.getElementsByTagName("head")[0].appendChild(meta);
+    window.ReactNativeWebView.postMessage(document.body.scrollHeight);
+    true;
+  `;
 
 }
 
