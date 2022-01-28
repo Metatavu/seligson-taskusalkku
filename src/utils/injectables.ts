3158db0c5cbcfe740f8f40e29e5b4d7a4a4bc9d1
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2022-01-28 12:28:06 +0200

    Added better solution to iOS zoom issue

--- src/utils/injectables.ts
+++ src/utils/injectables.ts
@@ -151,17 +151,27 @@ namespace Injectables {
   `;
 
   /**
-   * Temporary iOS zoom fix
-   *
-   * TODO: Add html elements which would solve this during render
+   * Returns injectable Javascript code for publication details
    */
-  export const iosWebviewZoomFix = `
-    const meta = document.createElement("meta");
-    meta.setAttribute("content", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1");
-    meta.setAttribute("name", "viewport");
-    document.getElementsByTagName("head")[0].appendChild(meta);
+  export const getPublicationsScript = () => `
     window.ReactNativeWebView.postMessage(document.body.scrollHeight);
     true;
+  `;
+
+  /**
+   * Returns html for publication details
+   *
+   * @param content content html string
+   */
+  export const getPublicationDetailsHtml = (content: string) => `
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1">
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
   `;
 
 }
