eba7b51d2202f603bb145487e27b661cc5b6a422
jarinykanen <jarinykanen.koodausta@gmail.com>
2022-03-16 10:55:03 +0200

    Added sentry configs

--- index.tsx
+++ index.tsx
@@ -1,6 +1,14 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 import App from "./App";
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  enableInExpoDevelopment: true,
+  debug: true,
+  attachStacktrace: true
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => Root);
 // It also ensures that whether you load the app in Expo Go or in a native build,
