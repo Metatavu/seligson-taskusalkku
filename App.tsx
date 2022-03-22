d10bae5f87829aabb7076120fbd49162b9debb89
Valmetti <vmvalonen@gmail.com>
2022-03-22 10:14:30 +0200

    fix for status bar visibility on iOS

--- App.tsx
+++ App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => (
       <ErrorHandler>
         <ApiProvider providers={ providers }>
           <AuthRefresh/>
-          <StatusBar/>
+          <StatusBar style="auto"/>
           <Root/>
         </ApiProvider>
       </ErrorHandler>
