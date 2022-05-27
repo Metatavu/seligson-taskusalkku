81cab32efd21fb96ba5c8abad793cd111a869bdb
Antti Leppä <antti.leppa@metatavu.fi>
2022-05-27 10:26:39 +0300

    Bumped version

--- app.config.ts
+++ app.config.ts
@@ -9,7 +9,7 @@ import "dotenv/config";
  * !!! DO NOT PUT ANY SECRETS TO ENVIRONMENT VARIABLES !!!
  */
 
-const version = "25.1.2";
+const version = "25.1.3";
 
 /**
  * Generates Android version code from version string
