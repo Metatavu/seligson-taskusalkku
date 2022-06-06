301381ed345bbe3f48443f21d1d88dca2886e3f1
Antti Leppä <antti.leppa@metatavu.fi>
2022-06-06 10:30:46 +0300

    Fixed issue with Android maual login

--- src/components/screens/auth/login-required-screen.tsx
+++ src/components/screens/auth/login-required-screen.tsx
@@ -5,7 +5,7 @@ import { Button } from "react-native-paper";
 import strings from "../../../localization/strings";
 import styles from "../../../styles/screens/auth/login-required-screen";
 import { SeligsonLogo } from "../../../../assets/seligson-logo";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { TouchableWithoutFeedback } from "react-native";
 import AuthUtils from "../../../utils/auth";
 import { useAppDispatch, useAppSelector, useHardwareGoBack, useTimeout } from "../../../app/hooks";
 import Config from "../../../app/config";
@@ -186,7 +186,9 @@ const LoginRequiredScreen: React.FC = () => {
       <View style={ styles.cardWrapper }>
         <View style={ styles.cardContent }>
           <TouchableWithoutFeedback onPress={ onHiddenLoginTriggerPress }>
-            <SeligsonLogo/>
+            <View>
+              <SeligsonLogo/>
+            </View>
           </TouchableWithoutFeedback>
           <Text style={ styles.titleText }>
             { strings.auth.loginRequired }
